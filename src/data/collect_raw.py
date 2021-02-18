3dfc2ed2eb5a43ec4864d3cc1bfe1adbb40f0d6b
Luke Van Cleve <lukevancleve@gmail.com>
2021-02-17 19:57:41 -0500

    changed files

--- src/data/collect_raw.py
+++ src/data/collect_raw.py
@@ -4,54 +4,77 @@ from os.path import exists
 import os
 from multiprocessing.pool import ThreadPool
 import numpy as np
-
-print("here")
+from hashlib import md5
 
 bv = pd.read_csv("./data/external/bill_version.csv", sep=";", encoding="latin1", parse_dates=True)
 import time
 start_time = time.time()
 
-os.mkdir('data/raw')
+if not os.path.exists('data/raw'):
+    os.mkdir('data/raw')
+if not os.path.exists('data/clean'):
+    os.mkdir('data/clean')
 
 
-def copy_local(i) -> int:
+def copy_local_and_clean(i):
     """
     Go to the (i)th index of the bill version csv file and download the file for local work.
     """
+
 
     try:
        file_name =  "./data/raw/"+str(bv['id'][i])+".txt" 
     except Exception:
         print("Index {i} is longer than the data (len { len(bv) }). ")
 
-
-    if exists(file_name):
-        return 0 # Correctly skipped
-
     if i % 10_000 == 0:
         print(" Iteration: {}".format(i))
         print(" --- %s seconds ---" % (time.time() - start_time))
 
-    url = bv['url'][i]
+    if not exists(file_name):
 
-    try:
+        url = bv['url'][i]
         br = raw_downloader.raw_downloader(url)
-
+        plain_text = br.plain_text
         text_file = open(file_name, "w")
-        rv = text_file.write(br.plain_text)
+        rv = text_file.write(plain_text)
         text_file.close()
-    except Exception:
-        print(f"Issue open/closing file from {url}.")
+    else:
+        with open(file_name) as f:
+            plain_text = f.read()
 
-    return 1  # Correctly downloaded
+
+
+    bp = raw_downloader.bill_processing()
+
+    ct = bp.clean_text(plain_text)
+    
+    ac = 0
+    if ct is None:
+        #print(str.encode(bv['url'][i]))
+
+        hash = md5()
+        hash.update(str.encode(bv['url'][i]))
+
+        url = "https://s3.amazonaws.com/statesavvy/" + hash.hexdigest()
+        #print( f"Access Denied for bill version {bv['id'][i]} at {url}" )
+        ac = 1
+    else:
+
+        file_name =  "./data/clean/"+str(bv['id'][i])+".txt"
+        with open(file_name, "w") as f:
+            rv = f.write(ct)
+        
+       
+    return tuple([1, ac])  
 
 
 tp = ThreadPool(processes=30)
 
-n_dl = tp.map(copy_local, range(len(bv['url'])))
+n_dl = tp.map(copy_local_and_clean, range(len(bv['url'])))
 
-print(f"Number of files downloaded: {np.sum(n_dl)}")
-print(f"Number of files already downloaded: {len(n_dl) - np.sum(n_dl)}")
+print(f"Number of files downloaded:  {np.sum([x[0] for x in n_dl])}")
+print(f"Number of files inaccesible: {np.sum([x[1] for x in n_dl])}")
 
 tp.terminate()
 tp.close()
--- src/data/raw_downloader.py
+++ src/data/raw_downloader.py
@@ -27,27 +27,88 @@ class raw_downloader():
         r = requests.get(self.plain_text_url)
         self.plain_text = r.text
 
-#    def clean_text(self):
-#
-#        # 0. Remove all the non-ascii characters
-#        clean = self.plain_text.encode("ascii", "ignore").decode()
-#
-#        # 1. Remove all the line numbers from bill text
-#        clean = re.sub('\n\s*\d*\s', '\n', clean)
-#
-#        # 2. fix between line dashes
-#        clean = re.sub('[-]\s*\n\s*', '', clean)
-#
-#        # 3. Remove (\d), (\d\d), or (\a)
-#        clean = re.sub('\(\d+\)', '', clean)
-#        clean = re.sub('\(\w+\)', '', clean)
-#
-#        # 4. Tokenize it
-#        tokens = word_tokenize(clean)
-#
-#        # save back to the class
-#        self.tokens = [word for word in tokens if word.isalpha()]
-#        self.clean_string = " ".join(self.tokens)
+class bill_processing():
+
+    def clean_text(self, text: str) -> str:
+
+
+
+        # 0. Remove all the non-ascii characters
+        clean = text.encode("ascii", "ignore").decode()
+
+
+        issue = clean.find('Access Denied')
+        if issue != -1:
+            return(None)
+
+        #
+        start_anchors = ["- FullText", "DELAWARE STATE SENATE", "STATE OF NEW HAMPSHIRE",
+        "Be it enacted by the General Assembly of the state of Missouri, as follows:",
+        "(Text matches printed bills. Document has been reformatted to meet World Wide Web specifications."]
+        for start_anchor in start_anchors:
+            ind = clean.find(start_anchor)
+            if ind != -1:
+                clean = clean[(ind+len(start_anchor)):]
+
+
+        end_anchors = ["----XX----"]
+        for end_anchor in end_anchors:
+            ind = clean.find(end_anchor)
+            if ind != -1:
+                clean = clean[:ind]
+
+        #return(clean)
+        # 1. Remove all the line numbers from bill text
+        clean = re.sub('\n\s*\d*\s', '\n', clean)
+
+        clean = re.sub('SB\d+ Enrolled.*?\\n', '', clean)
+
+        # remove line feed looking thing:
+        clean = re.sub('\s*=+\\n\s*LC\d*\s*\\n\s*=+\\n', '', clean)
+
+        # 2. fix between line dashes
+        clean = re.sub('[-]\s*\n\s*', '', clean)
+
+        # 3. Remove (\d), (\d\d), or (\a)
+        clean = re.sub('\(\d+\)', '', clean)
+        clean = re.sub('\(\w+\)', '', clean)
+
+        # 4. Remove markdownlinks:
+        clean = re.sub('\[.*?\]\(.*?\)', '', clean)
+
+        # 5. remove newlines
+        clean = re.sub('\\n', ' ', clean)
+
+        # 5. remove dashes following a space or preceding a space
+        clean = re.sub('[-_]+', '-', clean)
+        clean = re.sub(' [-_]', ' ', clean)
+        clean = re.sub('[-_] ', ' ', clean)
+
+        # collpse whitespce into one space.
+        clean = re.sub('\s+', ' ', clean)
+  
+        clean = re.sub('\\nSec\. \d.*?.\\n', "\n", clean)
+
+        clean = re.sub('\(.*?\)', '', clean)
+        clean = re.sub('\[.*?\]', '', clean)
+
+        clean = re.sub('\(Source.*?\)', '', clean)
+
+        clean = re.sub('[Ss]ection \d+', '', clean)
+        clean = re.sub('[hH]\.? [rR]\.? \d+', '', clean)
+
+        clean = re.sub(' [-.,\d]+ ', ' ', clean)
+
+        # lower case all
+        clean = clean.lower()
+
+        return(clean)
+        # 4. Tokenize it
+        #tokens = word_tokenize(clean)
+
+        # save back to the class
+        #self.tokens = [word for word in tokens if word.isalpha()]
+        #self.clean_string = " ".join(self.tokens)
 
 
 
